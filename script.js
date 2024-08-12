b0f8ad342a0fb24952e24511ba8c44ca5cc02b7c
Vinnie <vincentathorne2005@gmail.com>
2024-08-12 13:24:50 -0700

    slider

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
-let gameSpeed = 20;
+let gameSpeed = 5;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "img/layer-1.png";
@@ -15,6 +15,16 @@ backgroundLayer4.src = "img/layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "img/layer-5.png";
 
+const slider = document.getElementById("slider");
+slider.value = gameSpeed;
+const showGameSpeed = document.getElementById("showGameSpeed");
+showGameSpeed.innerHTML = gameSpeed;
+slider.addEventListener('change', function (e) {
+  gameSpeed = e.target.value;
+  showGameSpeed.innerHTML = gameSpeed;
+
+})
+
 class Layer{
   constructor(image, speedModifier) {
     this.x = 0;
@@ -22,6 +32,7 @@ class Layer{
     this.width = 2400;
     this.height = 700;
     this.x2 = this.width;
+    this.image = image;
     this.speedModifier = speedModifier;
     this.speed = gameSpeed * this.speedModifier;
 
@@ -40,14 +51,24 @@ class Layer{
   }
   draw() {
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
   }
 }
-//1:08:43
+
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
+  gameObjects.forEach(object => {
+    object.update();
+    object.draw();
+  });
   requestAnimationFrame(animate);
 }
 animate();
